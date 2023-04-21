5422459aa78dc71c5b8f80e8405b2558dd14c4e1
baran61 <azak.baran99@gmail.com>
2023-04-21 21:16:02 +0100

    refactoring

--- app/index.js
+++ app/index.js
@@ -1,15 +1,50 @@
 import { View, ScrollView, SafeAreaView } from 'react-native'
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Stack, useRouter } from 'expo-router'
 import { COLORS, icons, images, SIZES } from '../constants'
 import { Nearbyjobs, ScreenHeaderBtn, Welcome } from '../components'
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
+//The splashScreen is taken from https://docs.expo.dev/versions/latest/sdk/splash-screen/
+SplashScreen.preventAutoHideAsync();
 
 const Home = () => {
     const router = useRouter();
-    const [searchTerm, setsearchTerm] = useState("")
-    return (
+    const [searchTerm, setsearchTerm] = useState("");
+    const [appIsReady, setAppIsReady] = useState(false);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        
+        await Font.loadAsync(Entypo.font);
+        
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+    return (
         <SafeAreaView style={{
             flex: 1, backgroundColor: COLORS.lightWhite
         }}>
