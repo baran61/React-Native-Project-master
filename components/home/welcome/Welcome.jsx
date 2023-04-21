9a41eed30202c9830d109d73e71aa9ea3df1b29e
baran61 <azak.baran99@gmail.com>
2023-04-21 23:44:57 +0100

    refactroing

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,19 +1,49 @@
-import useState from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   View,
   Text,
   TextInput,
   TouchableOpacity,
   Image,
-  FlatList
+  FlatList,
+  Platform,
+  StyleSheet
 } from 'react-native';
 import { useRouter } from 'expo-router';
 import styles from './welcome.style';
 import {icons, SIZES} from '../../../constants';
+import * as Location from 'expo-location';
+
 
 const Welcome = ({searchTerm, setSearchTerm, handleClick}) => {
   const router = useRouter();
-  
+
+  //For Geolocation https://docs.expo.dev/versions/latest/sdk/location/
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+
   return (
     <View>
       <View style={styles.container}>
@@ -30,8 +60,8 @@ const Welcome = ({searchTerm, setSearchTerm, handleClick}) => {
             type="search"
             placeholder="What are you looking for ?"
           />
-        </View>
 
+        </View>
         <TouchableOpacity style={styles.searchBtn} onPress= {handleClick}>
           <Image
             source={icons.search}
@@ -40,6 +70,17 @@ const Welcome = ({searchTerm, setSearchTerm, handleClick}) => {
           />
         </TouchableOpacity>
       </View>
+      
+      <View style={styles.container}>
+        <Text style={styles.userName}> Your Current Location is :</Text>
+        <Text style={styles.paragraph}>{text}</Text> 
+      </View>
+
+      
+
+      
+    
+
     </View>
   )
 }
