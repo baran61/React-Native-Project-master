e89380b2630f0a1860ba0c321c20def845cb68d6
baran61 <azak.baran99@gmail.com>
2023-04-25 15:27:14 +0100

    refactroing

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { useRouter } from "expo-router";
 import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import {
+  Button,
+  Platform,
+  Vibration,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
 import styles from './nearbyjobs.style'
 import { images } from "../../../constants";
 import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
 
 const Nearbyjobs = () => {
+
+  const ONE_SECOND_IN_MS = 1000;
+
+  const PATTERN = [
+    1 * ONE_SECOND_IN_MS,
+    2 * ONE_SECOND_IN_MS,
+    3 * ONE_SECOND_IN_MS,
+  ];
+
+  const PATTERN_DESC =
+    Platform.OS === 'android'
+      ? 'wait 1s, vibrate 2s, wait 3s'
+      : 'wait 1s, vibrate, wait 2s, vibrate, wait 3s';
+
+
   return (
     <View>
       <View style={styles.container}>
         <Text style={styles.headerTitle}>All Holidays are listed below.</Text>
+        
       </View>
 
       <View>
@@ -21,26 +44,31 @@ const Nearbyjobs = () => {
       <View>
         <NearbyJobCard photoUrl={images.antalya}></NearbyJobCard>
         <Text style={styles.headerTitle}>Turkey, Antalya </Text>
+        <Button title="See the details." onPress={() => Vibration.vibrate()} />
       </View>
 
       <View>
         <NearbyJobCard photoUrl={images.bodrum}></NearbyJobCard>
         <Text style={styles.headerTitle}>Turkey, Bodrum </Text>
+        <Button title="See the details." onPress={() => Vibration.vibrate()} />
       </View>
 
       <View>
         <NearbyJobCard photoUrl={images.izmir}></NearbyJobCard>
         <Text style={styles.headerTitle}>Turkey, İzmir </Text>
+        <Button title="See the details." onPress={() => Vibration.vibrate()} />
       </View>
 
       <View>
         <NearbyJobCard photoUrl={images.istanbul}></NearbyJobCard>
         <Text style={styles.headerTitle}>Turkey, İstanbul </Text>
+        <Button title="See the details." onPress={() => Vibration.vibrate()} />
       </View>
 
       <View>
         <NearbyJobCard photoUrl={images.ankara}></NearbyJobCard>
         <Text style={styles.headerTitle}>Turkey, Ankara </Text>
+        <Button title="See the details." onPress={() => Vibration.vibrate()} />
       </View>
 
     </View>
